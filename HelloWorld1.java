d4826bae8542fa3ba805fecbc235282ccb61a279
NeverSu <suxin0960@mail.sim.ac.cn>
2017-03-22 12:33:02 +0800

    20170322

---
+++ HelloWorld1.java
@@ -0,0 +1,25 @@
+public class HelloWorld1{
+    public static void main(String[] args) {
+	    int one = 10 ;
+        int two = 20 ;
+        int three = 0 ;
+        int three=one+two;
+        System.out.println("three=one+two==>"+three);
+        int three+=one;
+        System.out.println("three+=one==>"+three);
+        int three-=one;
+        System.out.println("three-=one==>"+three);
+        int three*=one;
+        System.out.println("three*=one==>"+three);
+        int three/=one;
+        System.out.println("three/=one==>"+three);
+        int three%=one;
+        System.out.println("three%=one==>"+three);
+        
+        
+        
+        
+        
+    
+	}
+}
